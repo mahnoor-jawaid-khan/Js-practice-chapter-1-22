1483a2810d1a5f4625ca1ecf7420eebdcb7df9c1
mahnoor-jawaid-khan <mahnoorkhan1034@gmail.com>
2022-09-05 06:46:49 -0700

    JavaScript chapter 1-22 practice

---
+++ app.js
@@ -0,0 +1,144 @@
+    // [alert("Welcome to the JS Project");
+// var a = 1, b = 5, c = a+b;
+// alert(c);
+
+// var oldnum = 10;
+// var newnum = oldnum + 20;
+// alert(newnum);
+
+
+// Marksheet (iF-Else)
+
+//  var marks = +prompt("Enter Your number to check the grade");
+
+//  if(marks >= 80)
+//  {
+//     alert("Your grade is A");
+//  }
+
+//  else if (marks >= 70)
+//  {
+//     alert("Your grade is B");
+
+//  }
+
+//  else if (marks >= 60)
+//  {
+//     alert("Your grade is c");
+
+//  }
+
+//  else if (marks >= 50)
+//  {
+//     alert("Your grade is D");
+
+//  }
+
+//  else if (marks < 50)
+//  {
+//     alert("Ohhh!! you're failed");
+
+//  }
+
+
+// Arrays
+
+// var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+//     alert(cities);
+ 
+// //Assigning value to empty array
+
+// var n = [];
+// n[0] = "Mahnoor";
+// n[1] = "Taimoor";
+// n[2] = "Areesha";
+// n[3] = "Ali";
+// alert(n);
+
+// n.pop(); //FOR REMOVING LAST INDEX OF AN ARRAY
+// alert(n);
+
+// n.shift(); //FOR REMOVING first INDEX OF AN ARRAY
+// alert(n);
+
+// n.push("Aiman");  //For Adding LAST INDEX OF AN ARRAY
+// alert(n);
+
+// n.unshift("maryam", "Salar"); //FOR adding first INDEX OF AN ARRAY
+// alert(n);
+
+// n.splice(0,1,"Khan"); //used to add or remove value from which index you want.
+// alert(n);
+
+// var ncopy = n.slice(0,2); //used to copy value from one index to another
+// alert(ncopy);
+
+
+//For Loop
+
+//printing multiplication table of 2
+// for(var i = 1; i <= 10; i++)
+// {
+//     document.write("2 x " + i + " = " +i*2 + "<br>");
+// }
+
+
+
+//printing multiplication table by taking input
+
+// var num = +prompt("Enter An integer number"); // taking number
+// var range = +prompt("Enter range"); // taking range
+
+// for(var i=1; i<=range ; i++)
+// {
+//     document.write(num + " x " + i + " = " + num * i + "<br>");
+// }
+
+//NESTED LOOP
+
+//printing side triangle
+// for(j
+
+//printing downward triangle
+
+// var n = 5;
+// for(var i = 0; i <= n; i++ ){
+//     for(var j = 0; j<=n-i; j++ ){
+//         document.write(" * ");
+//     }
+//   document.write("<br>");
+// }
+
+// changing case
+ 
+//TO UPPER Case
+// var students = "mahnoor khan"
+//  students = students.toUpperCase();
+// document.write(students);
+
+// // to lower case
+// var students = "MAHNOOR KHAN"
+//  students = students.toLowerCase();
+// document.write(students);
+
+
+// chapter # 22 string length
+
+//ex # finding string
+// var student = "MAHNOOR KHAN";
+// student = student.length;
+// console.log(student);
+
+// ex # finding string length by taking input by user
+// var sentence = prompt("Enter a sentence");
+// document.write(sentence);
+//  sentence = sentence.length;
+//  document.write("<br> The length of your sentence is " + sentence);
+
+
+
+
+
+
+
+
